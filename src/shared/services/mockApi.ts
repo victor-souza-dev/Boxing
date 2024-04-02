8b384318f7206b723a496eaf10937653d2fe3336
Victor Souza <victor.souza@playhub.com.br>
2024-04-02 19:02:05 -0300

    refactor: change baseUrl to mock

--- src/shared/services/mockApi.ts
+++ src/shared/services/mockApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const mockApi = axios.create({
-  baseURL: "/mock",
+  baseURL: "https://boxing-one.vercel.app/mock",
   headers: {
     "Content-type": "application/json",
   },
